57fe4af948284c5f386f6a41cc72c4234e23af5a
JegoLazaro <jose_gabriel_lazaro@dlsu.edu.ph>
2023-11-23 20:11:27 +0800

    Fixed mobile responsiveness

--- src/components/headerComp.jsx
+++ src/components/headerComp.jsx
@@ -4,18 +4,18 @@ import SkillCards from './skillsComponents/skillCards'
 const HeaderComp = ({title, description, reverse}) => {
 
   return (
-    <div id='skills' className={`bg-white py-24 sm:py-32`}>
+    <div id='skills' className={`bg-white py-20 sm:py-32`}>
     <div className={`mx-auto max-w-7xl px-6 lg:px-8 flex ${reverse? "justify-end" : "justify-start" }`}>
       <div className="max-w-2xl">
         <h2 className={`text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl flex ${reverse? "justify-end" : "justify-start" } `}>
           {title}
         </h2>
-        <p className="mt-2 text-lg leading-8 text-gray-600">
+        <p className="mt-2  mb-4 text-lg leading-8 text-gray-600">
           {description}
         </p>
       </div>
     </div>
-    <div className="mx-auto grid max-w-2xl grid-rows-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-rows-3">
+    <div className="mx-auto grid max-w-2xl grid-rows-2 gap-x-8  border-t-2 border-gray-200 lg:pt-48 md:pt-40 sm:mt-1 sm:pt-1 lg:mx-2 lg:max-w-none lg:grid-rows-3">
       {/* Add your grid content here if needed */}
       
     </div>
--- src/components/skillsComponents/skillCards.jsx
+++ src/components/skillsComponents/skillCards.jsx
@@ -1,54 +1,66 @@
-import React from 'react'
-import assets from '../../assets';
+import React from "react";
+import assets from "../../assets";
 import styles from "../../assets/Globals";
 
-
-export const FeatureCard = ({iconUrl,iconText}) => (
-    <div className={`${styles.featureCard} m-1`}>
-      <img src={iconUrl} alt='icon' className={`${styles.featureImg}`}/>
-      <p className={`${styles.featureText}`}>{iconText}</p>
-    </div>
-  )
+export const FeatureCard = ({ iconUrl, iconText }) => (
+  <div className={`${styles.featureCard} m-1`}>
+    <img src={iconUrl} alt="icon" className={`${styles.featureImg}`} />
+    <p className={`${styles.featureText}`}>{iconText}</p>
+  </div>
+);
 
 const SkillCards = () => {
-    const skillsData = [
-        { iconUrl: assets.html_logo, iconText: "HTML 5", link: '' },
-        { iconUrl: assets.javascript, iconText: "Javascript", link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
-        { iconUrl: assets.css_logo, iconText: "CSS 3", link: '' },
-        { iconUrl: assets.react_logo, iconText: "React JS", link: 'https://expo.dev/' },
-        
-        { iconUrl: assets.react, iconText: "React Native", link: 'https://reactnative.dev/' },
-        { iconUrl: assets.tailwind, iconText: "Tailwind CSS", link: '' },
-        { iconUrl: assets.expo02, iconText: "Expo", link: '' },
-        { iconUrl: assets.figma_logo, iconText: "Figma", link: '' },
+  const skillsData = [
+    { iconUrl: assets.html_logo, iconText: "HTML 5", link: "" },
+    {
+      iconUrl: assets.javascript,
+      iconText: "Javascript",
+      link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    },
+    { iconUrl: assets.css_logo, iconText: "CSS 3", link: "" },
+    {
+      iconUrl: assets.react_logo,
+      iconText: "React JS",
+      link: "https://expo.dev/",
+    },
 
-        { iconUrl: assets.git, iconText: "Git", link: '' },
-        { iconUrl: assets.nodejs, iconText: "NodeJS", link: '' },
-        { iconUrl: assets.python, iconText: "Python", link: '' },
-        { iconUrl: assets.react_bootstrap, iconText: "React-Bootstrap", link: '' },
-        { iconUrl: assets.bootstrap, iconText: "Bootstrap", link: '' },
-        // Add more objects as needed
-      ];
-      
+    {
+      iconUrl: assets.react,
+      iconText: "React Native",
+      link: "https://reactnative.dev/",
+    },
+    { iconUrl: assets.tailwind, iconText: "Tailwind CSS", link: "" },
+    { iconUrl: assets.expo02, iconText: "Expo", link: "" },
+    { iconUrl: assets.figma_logo, iconText: "Figma", link: "" },
+
+    { iconUrl: assets.git, iconText: "Git", link: "" },
+    { iconUrl: assets.nodejs, iconText: "NodeJS", link: "" },
+    { iconUrl: assets.python, iconText: "Python", link: "" },
+    { iconUrl: assets.react_bootstrap, iconText: "React-Bootstrap", link: "" },
+    { iconUrl: assets.bootstrap, iconText: "Bootstrap", link: "" },
+    // Add more objects as needed
+  ];
 
   return (
     <div>
-         <div>
-  <div className={`${styles.section} lg:-mt-80 md:-mt-64 sm:-mt-22`}>
-    <div className={`${styles.subSection} flex-col text-center`}>
-      <div className={styles.flexWrap}>
-        {skillsData.map((skill, index) => (
-          <a key={index} href={skill.link}>
-            <FeatureCard iconUrl={skill.iconUrl} iconText={skill.iconText} />
-          </a>
-        ))}
+      <div>
+        <div className={`${styles.section} lg:-mt-72 md:-mt-44 sm:-mt-22`}>
+          <div className={`${styles.subSection} flex-col text-center`}>
+            <div className={styles.flexWrap}>
+              {skillsData.map((skill, index) => (
+                <a key={index} href={skill.link}>
+                  <FeatureCard
+                    iconUrl={skill.iconUrl}
+                    iconText={skill.iconText}
+                  />
+                </a>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
+  );
+};
 
-    </div>
-  )
-}
-
-export default SkillCards
+export default SkillCards;
